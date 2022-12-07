8f91d26b84ba5067d4343d68913bd42b5072e31e
huynhnam249 <67179307+huynhnam249@users.noreply.github.com>
2022-12-07 09:00:34 +0700

    change database link connect

--- database/config.js
+++ database/config.js
@@ -3,14 +3,14 @@ require('dotenv').config()
 const Pool = require('pg').Pool
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const connectionString = `postgres://api_user:oerdiRm12PjfUNdmgpJhKj7qRU4urzWT@dpg-ce7sfso2i3mvm01u7af0-a/auth_poa3`
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: {
-    rejectUnauthorized: false
-  }
-  // ssl: process.env.DATABASE_URL ? true : false
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+    // ssl: process.env.DATABASE_URL ? true : false
 })
 
 module.exports = { pool }
